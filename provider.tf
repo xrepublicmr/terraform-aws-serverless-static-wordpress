5e2030dd8fdae40d067e164377689daf80ec1e86
Elvis Barah <enjbarah@gmail.com>
2021-08-01 23:06:17 -0600

    reverted changes after upgrading terraform to 1.03

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 terraform {
-  required_version = ">= 0.14.8"
+  required_version = ">= 0.15.1"
   required_providers {
     aws = {
       source = "hashicorp/aws"
       # https://github.com/hashicorp/terraform-provider-aws/blob/main/CHANGELOG.md
       version               = "~> 3.0"
-      # configuration_aliases = [aws.ue1]
+      configuration_aliases = [aws.ue1]
     }
     random = {
       source = "hashicorp/random"
